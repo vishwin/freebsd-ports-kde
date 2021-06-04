4f5f435965bc5286ef89f3d722e4cbadb7ed0d01
Tobias C. Berner <tcberner@FreeBSD.org>
2021-06-04 15:41:35 +0200

    plasma5: update to 5.22.0

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -75,8 +75,8 @@ _KDE_RELNAME=		KDE${_KDE_VERSION}
 
 # === VERSIONS OF THE DIFFERENT COMPONENTS =====================================
 # Current KDE desktop.
-KDE_PLASMA_VERSION?=		5.21.90
-KDE_PLASMA_BRANCH?=		unstable
+KDE_PLASMA_VERSION?=		5.22.0
+KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
 KDE_FRAMEWORKS_VERSION?=	5.82.0
